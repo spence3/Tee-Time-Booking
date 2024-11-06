4d4bd653da4c6961405d3bf35ad674722d6c712f
Spencer Miller <spencerrmiller3@gmail.com>
2024-11-05 19:43:41 -0700

    displaying times

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,25 @@
 const teeTimeAPI = '/api/v1/teetimes'
 
 $(function () {
-    function getTeeTimes() {
-        $.ajax({
-          url: teeTimeAPI,
-          method: 'GET',
-          success: function (data) {
-            $.each(data, function (i, test) {
-              console.log(test.time)
-            });
-          },
-          error: function (err) {
-            alert(err);
-          },
-        });
-      }
-    getTeeTimes()
+  function getTeeTimes(timeList) {
+      $.ajax({
+        url: teeTimeAPI,
+        method: 'GET',
+        success: function (data) {
+          var list = $(timeList)
+          $.each(data, function (i, time) {
+            var data = $('<li></li>').text(time.time)
+            list.append(data)
+          });
+        },
+        error: function (err) {
+          alert(err);
+        },
+      });
+    }
+    $('#sleepy').on('click', function(){
+      getTeeTimes('#sleepyList')
+    })
 })
+
+//
--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.static(path.join(__dirname,'public')))
 const [year, month, day] = new Date().toISOString().split('T')[0].split('-');
 
 app.get('/api/v1/teetimes', (req, res) => {
-    axios.get(`https://foreupsoftware.com/index.php/api/booking/times?time=all&date=${month}-${day}-${year}&holes=all&players=0&booking_class=3412&schedule_id=1726&schedule_ids%5B%5D=1726&specials_only=0&api_key=no_limits`)
+    //https://foreupsoftware.com/index.php/api/booking/times?time=all&date=11-06-2024&holes=all&players=0&booking_class=3412&schedule_id=1726&schedule_ids%5B%5D=1726&specials_only=0&api_key=no_limits
+    axios.get(`https://foreupsoftware.com/index.php/api/booking/times?time=all&date=${month}-06-${year}&holes=all&players=0&booking_class=3412&schedule_id=1726&schedule_ids%5B%5D=1726&specials_only=0&api_key=no_limits`)
         .then((response) => {
             // console.log(response.data); // Log the entire response data
             // Check if it's JSON or HTML
