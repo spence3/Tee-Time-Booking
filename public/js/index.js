26a0492066377155d9ca3d222ce8592ceed44479
Spencer Miller <spencerrmiller3@gmail.com>
2024-11-09 16:02:04 -0700

    shows no tee times if none available

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,32 @@ $(function () {
       success: function (data) {
         var button = $(buttonTag)
         var ul = $(timeList)
-        $.each(data, function (i, data) {
-          console.log('in here')
-          //switching army to standard
-          var armyTime = data.time.split(' ')
-          var standardTime = moment(armyTime[1], 'HH:mm').format('h:mm A');
-          var li = $('<li></li>')
-          //var data = $('<a class="block text-center w-96 px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white hover: cursor-pointer"></a>').text(time.time)
-          var book = $(`<a href="${URL}" class="flex flex-row justify-evenly p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">` +
-                        `<h5 class="px-4 dark:text-white">Time: ${standardTime}</h5>` +
-                        `<p class="px-4 dark:text-white">Minimum Players: ${data.minimum_players}</p>` +
-                        ` <p class="px-4 dark:text-white">Maximum Players: ${data.maximum_players_per_booking}</p>` +
-                        `<p class="px-4 dark:text-white">Holes: ${data.holes}</p>` +
-                        `<p class="px-4 dark:text-white">Available Spots: ${data.available_spots}</p>`
+        var li = $('<li></li>')
+        var bookData
+        if(data.length > 0){
+          $.each(data, function (i, data) {
+            //switching army to standard
+            var armyTime = data.time.split(' ')
+            var standardTime = moment(armyTime[1], 'HH:mm').format('h:mm A');
+            var bookData = $(`<a href="${URL}" class="flex flex-row justify-evenly p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">` +
+                          `<h5 class="px-4 dark:text-white">Time: ${standardTime}</h5>` +
+                          `<p class="px-4 dark:text-white">Minimum Players: ${data.minimum_players}</p>` +
+                          `<p class="px-4 dark:text-white">Maximum Players: ${data.maximum_players_per_booking}</p>` +
+                          `<p class="px-4 dark:text-white">Holes: ${data.holes}</p>` +
+                          `<p class="px-4 dark:text-white">Available Spots: ${data.available_spots}</p>`
+            )
+            li.append(bookData)
+            ul.append(li)
+          })
+        }
+        else{
+          ul.empty()
+          var bookData = $('<div class="flex flex-row justify-evenly p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">' +
+                            '<h1>No Times Available</h1>'
           )
-          li.append(book)
+          li.append(bookData)
           ul.append(li)
-        });
+        }
         
         var dropdown = $(dropDownTag);
         dropdown.removeClass('hidden');
@@ -76,6 +85,6 @@ $(function () {
     const oaksAPI = '/api/v1/oaks'
     var dropdown = $('#oaksDropDown')
     //Call it as an async
-    await dropDownShow('#oaks', dropdown,'#oaksList', oaksAPI, 'https://foreupsoftware.com/index.php/booking/6279/49#/teetimes')
+    await dropDownShow('#oaks', dropdown,'#oaksList', oaksAPI, 'https://foreupsoftware.com/index.php/booking/21698/8633#teetimes')
   })
 })
