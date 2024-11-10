e952ea747e9bb3a0cbc08293e6cb7d723912ed74
Spencer Miller <spencerrmiller3@gmail.com>
2024-11-09 17:34:21 -0700

    adding soldier hollow

--- public/js/index.js
+++ public/js/index.js
@@ -8,8 +8,8 @@ $(function () {
         $(buttonTag).append('<svg class="animate-spin h-5 w-5 mr-3 text-white" viewBox="0 0 24 24"> <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" stroke-dasharray="31.4" stroke-dashoffset="0" fill="none"></circle> </svg>')
       },
       success: function (data) {
-        var button = $(buttonTag)
         var ul = $(timeList)
+        ul.empty()//empty list before adding
         var li = $('<li></li>')
         var bookData
         if(data.length > 0){
@@ -18,16 +18,18 @@ $(function () {
             var armyTime = data.time.split(' ')
             var standardTime = moment(armyTime[1], 'HH:mm').format('h:mm A');
             var bookData = $(`<a href="${URL}" class="flex flex-row justify-evenly p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">` +
-                          `<h5 class="px-4 dark:text-white">Time: ${standardTime}</h5>` +
-                          `<p class="px-4 dark:text-white">Minimum Players: ${data.minimum_players}</p>` +
-                          `<p class="px-4 dark:text-white">Maximum Players: ${data.maximum_players_per_booking}</p>` +
-                          `<p class="px-4 dark:text-white">Holes: ${data.holes}</p>` +
-                          `<p class="px-4 dark:text-white">Available Spots: ${data.available_spots}</p>`
+                              `<h5 class="px-4 dark:text-white">Time: ${standardTime}</h5>` +
+                              `<p class="px-4 dark:text-white">Minimum Players: ${data.minimum_players}</p>` +
+                              `<p class="px-4 dark:text-white">Maximum Players: ${data.maximum_players_per_booking}</p>` +
+                              `<p class="px-4 dark:text-white">Holes: ${data.holes}</p>` +
+                              `<p class="px-4 dark:text-white">Available Spots: ${data.available_spots}</p>` +
+                            `</a>`
             )
             li.append(bookData)
             ul.append(li)
           })
         }
+        //no times available
         else{
           ul.empty()
           var bookData = $('<div class="flex flex-row justify-evenly p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">' +
@@ -36,7 +38,6 @@ $(function () {
           li.append(bookData)
           ul.append(li)
         }
-        
         var dropdown = $(dropDownTag);
         dropdown.removeClass('hidden');
       },
@@ -50,7 +51,7 @@ $(function () {
     });
   }
 
-  //Make Async
+ //hide/show dropdown
   async function dropDownShow(buttonTag, dropdown, timeList, API, URL){
     try{
       const isVisible = !dropdown.hasClass('hidden'); // Check if it's already visible
@@ -66,25 +67,28 @@ $(function () {
     }
   }
      
-
+  //drop down buttons for teetimes
   $('#sleepy').on('click', async function(){
     const sleepyAPI = '/api/v1/sleepy'
     var dropdown = $('#sleepyDropDown')
-    //Call it as an async
     await dropDownShow('#sleepy', dropdown,'#sleepyList', sleepyAPI, 'https://foreupsoftware.com/index.php/booking/19396/1726#/teetimes')
   })
 
   $('#timp').on('click', async function(){
     const timpAPI = '/api/v1/timp'
     var dropdown = $('#timpDropDown')
-    //Call it as an async
     await dropDownShow('#timp', dropdown,'#timpList', timpAPI, 'https://foreupsoftware.com/index.php/booking/6279/49#/teetimes')
   })
 
   $('#oaks').on('click', async function(){
     const oaksAPI = '/api/v1/oaks'
     var dropdown = $('#oaksDropDown')
-    //Call it as an async
     await dropDownShow('#oaks', dropdown,'#oaksList', oaksAPI, 'https://foreupsoftware.com/index.php/booking/21698/8633#teetimes')
+  })
+
+  $('#soldier').on('click', async function(){
+    const soldierAPI = 'api/v1/soldier-hollow'
+    var dropdown = $('#soldierDropDown')
+    await dropDownShow('#soldier', dropdown,'#soldierList', soldierAPI, 'https://foreupsoftware.com/index.php/booking/21698/8633#teetimes')
   })
 })
--- server.js
+++ server.js
@@ -3,52 +3,89 @@ const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
-app.use(express.static('public'));
+app.use(express.static('public'))
 const path = require('path')
 
 //cors
-const cors = require('cors');
-app.use(cors());
+const cors = require('cors')
+app.use(cors())
 
 //set up static folder
 app.use(express.static(path.join(__dirname,'public')))
 
 //organize date
-const [year, month, day] = new Date().toISOString().split('T')[0].split('-');
+const [year, month, day] = new Date().toISOString().split('T')[0].split('-')
 console.log(year, month, day)
 
 app.get('/api/v1/sleepy', (req, res) => {
-    axios.get(`https://foreupsoftware.com/index.php/api/booking/times?time=all&date=${month}-${day}-${year}&holes=all&players=0&booking_class=3412&schedule_id=1726&schedule_ids%5B%5D=1726&specials_only=0&api_key=no_limits`)
+    axios.get(`https://foreupsoftware.com/index.php/api/booking/times?time=all&date=${month}-10-${year}&holes=all&players=0&booking_class=3412&schedule_id=1726&schedule_ids%5B%5D=1726&specials_only=0&api_key=no_limits`)
         .then((response) => {
-            res.json(response.data); // Return the data back to the client
+            res.json(response.data) // Return the data back to the client
         })
         .catch((error) => {
-            console.error('Error fetching booking data:', error);
-            res.status(500).json({ error: 'Failed to fetch booking data' });
-        });
-});
+            console.error('Error fetching booking data:', error)
+            res.status(500).json({ error: 'Failed to fetch booking data' })
+        })
+})
 
 app.get('/api/v1/timp', (req, res) => {
-    axios.get(`https://foreupsoftware.com/index.php/api/booking/times?time=all&date=${month}-${day}-${year}&holes=all&players=0&booking_class=14927&schedule_id=49&schedule_ids%5B%5D=49&schedule_ids%5B%5D=1973&schedule_ids%5B%5D=7542&specials_only=0&api_key=no_limits`)
+    axios.get(`https://foreupsoftware.com/index.php/api/booking/times?time=all&date=${month}-10-${year}&holes=all&players=0&booking_class=14927&schedule_id=49&schedule_ids%5B%5D=49&schedule_ids%5B%5D=1973&schedule_ids%5B%5D=7542&specials_only=0&api_key=no_limits`)
         .then((response) => {
-            res.json(response.data); // Just send the raw data for now
+            res.json(response.data) // Just send the raw data for now
         })
         .catch((error) => {
-            console.error('Error fetching booking data:', error);
-            res.status(500).json({ error: 'Failed to fetch booking data' });
-        });
-});
+            console.error('Error fetching booking data:', error)
+            res.status(500).json({ error: 'Failed to fetch booking data' })
+        })
+})
 
 app.get('/api/v1/oaks', (req, res) => {
-    axios.get(`https://foreupsoftware.com/index.php/api/booking/times?time=all&date=${month}-${day}-${year}&holes=all&players=0&booking_class=10949&schedule_id=8633&schedule_ids%5B%5D=8633&specials_only=0&api_key=no_limits`)
+    axios.get(`https://foreupsoftware.com/index.php/api/booking/times?time=all&date=${month}-10-${year}&holes=all&players=0&booking_class=10949&schedule_id=8633&schedule_ids%5B%5D=8633&specials_only=0&api_key=no_limits`)
         .then((response) => {
-            res.json(response.data); // Just send the raw data for now
+            res.json(response.data) // Just send the raw data for now
         })
         .catch((error) => {
-            console.error('Error fetching booking data:', error);
-            res.status(500).json({ error: 'Failed to fetch booking data' });
-        });
-});
+            console.error('Error fetching booking data:', error)
+            res.status(500).json({ error: 'Failed to fetch booking data' })
+        })
+})
+
+app.get('/api/v1/soldier-hollow', (req, res) => {
+    console.log('made it hear')
+    axios.get("https://phx-api-be-east-1b.kenna.io/v2/tee-times?date=2024-11-11&facilityIds=17072,17073", {
+        "headers": {
+          "accept": "application/json, text/plain, */*",
+          "accept-language": "en-US,enq=0.9",
+          "cache-control": "no-cache",
+          "pragma": "no-cache",
+          "priority": "u=1, i",
+          "sec-ch-ua": "\"Chromium\"v=\"130\", \"Google Chrome\"v=\"130\", \"Not?A_Brand\"v=\"99\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"macOS\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "cross-site",
+          "x-be-alias": "aspira-management-company",
+          "Referer": "https://aspira-management-company.book-v2.teeitup.golf/",
+          "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+      })
+      .then((response) => {
+        data = {
+            time: response.data[1].teetimes.teetime,
+            minimum_players: response.data[1].teetimes.minPlayers,
+            maximum_players_per_booking: response.data[1].teetimes.maxPlayers,
+            // holse: response.data[1].teetimes
+        }
+        res.json(response.data[1].teetimes)
+      })
+      .catch((error) => {
+        console.error('Error fetching booking data:', error)
+        res.status(500).json({ error: 'Failed to fetch booking data' })
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
